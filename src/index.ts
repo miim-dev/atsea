2c545389731fa1c6cf10323aef588eb7955009d1
Benjamin Falk <benjamin.falk@cyanjoe.com>
2019-09-23 16:45:03 +0200

    add semi area rendering

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,49 @@
 import logger from './lib/logger';
 import keyevents from './lib/keyevents';
 
+import Area from './lib/Area';
+
+const atsea = {
+  Area,
+  render: ( areas: Area | Area[] ) => {
+    let arrayedAreas: Area[] = [];
+    if ( Array.isArray( areas ) ) {
+      arrayedAreas = areas;
+    }
+    else {
+      arrayedAreas = [ areas ];
+    }
+
+    arrayedAreas.forEach( ( area: Area ) => {
+      area.render();
+    } );
+  },
+};
+
 const port = 4921;
 const options = {
   port: 0,
   listen: 0,
 };
+let mainTest = false;
 
 if ( process.argv.indexOf( '--debug-listener' ) > -1 ) {
   options.listen = port;
 }
 else {
   options.port = port;
+  mainTest = true;
 }
 logger.init( options );
 
-keyevents.init();
+if ( mainTest ) {
+  // keyevents.init();
+  const a = new atsea.Area( {
+    size: {
+      width: 5,
+      height: 3,
+    },
+  } );
+
+  atsea.render( [ a ] );
+}
---
+++ src/lib/Area.ts
@@ -0,0 +1,53 @@
+import { Color } from './Style';
+
+export interface AreaOptions {
+  size?: AreaSize;
+  background?: Color;
+}
+
+export interface AreaSize {
+  width?: number | string;
+  height?: number | string;
+}
+
+export interface AreaRealSize {
+  cols: number;
+  rows: number;
+}
+
+class Area {
+  private options: AreaOptions;
+  private size: AreaSize = {
+    width: 0,
+    height: 0,
+  };
+  private realSize: AreaRealSize;
+
+  constructor( options: AreaOptions ) {
+    this.options = options;
+    this.size = options.size || this.size;
+  }
+
+  calculateRealSize() {
+    this.realSize = {
+      cols: this.size.width as number,
+      rows: this.size.height as number,
+    }
+  }
+
+  render() {
+    this.calculateRealSize();
+
+    for ( let y = 0; y < this.realSize.rows; y += 1 ) {
+      for ( let x = 0; x < this.realSize.cols; x += 1 ) {
+        process.stdout.write( 'x' );
+      }
+
+      if ( y < this.realSize.rows - 1 ) {
+        process.stdout.write( '\n' );
+      }
+    }
+  }
+}
+
+export default Area;
